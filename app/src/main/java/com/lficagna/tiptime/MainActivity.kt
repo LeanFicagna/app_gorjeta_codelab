2b15ac159a840901021576d8e987733f123a4d9d
LeanFicagna <leandersonficagna321@gmail.com>
2021-10-04 15:07:47 -0300

    first commit

---
+++ app/src/main/java/com/lficagna/tiptime/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.lficagna.tiptime
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.lficagna.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+import kotlin.math.ceil
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val price: Double? = binding.costOfServiceEditText.text.toString().toDoubleOrNull()
+        if (price == null) {
+            binding.tipResult.text = ""
+            return
+        }
+        val percent: Double = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        val total: Double = if (binding.roundUpSwitch.isChecked) ceil(price * percent) else price * percent
+        val formattedTip = NumberFormat.getCurrencyInstance().format(total)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+}
